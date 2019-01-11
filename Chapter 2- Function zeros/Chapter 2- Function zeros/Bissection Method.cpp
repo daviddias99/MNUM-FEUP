9955f0c2dcad46af1231233290136029a3ab18ed
daviddias99 <32851900+daviddias99@users.noreply.github.com>
2019-01-11 11:53:36 +0000

    Added Bissection Method

---
+++ Chapter 2- Function zeros/Chapter 2- Function zeros/Bissection Method.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double func(double x ){
+
+	return sin(x) + pow(x, 5) - 0.2*x + 0.5;
+
+}
+
+void bissection(double f(double), double limA, double limB, double nIter ) {
+
+	double a = limA, b = limB, m;
+
+	for (int i = 0; i <= nIter; i++) {
+
+		m = (a + b) / 2;
+
+		cout << setprecision(10) << i+1 << "- " << "a: " << setw(15) << a << " b: " << setw(15) << b << " m: " << setw(15) << m << " fa: " << setw(15) << f(a) << " fb: " << setw(15) << f(b) << " fm:  " << setw(15) << f(m) << endl;
+		
+		if (f(a)*f(m) < 0)
+			b = m;
+		else
+			a = m;
+
+	}
+
+}
+
+
+
+int main()
+{
+	bissection(func, -1, 0, 6);
+}
+
